af28f295762b3d514fbdbec161e508709b56f803
Mark Raffy <markraffybahalla@gmail.com>
2025-04-11 01:28:14 +0800

    Add links to social icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,19 +7,27 @@ import WFCLogoWhite from "@/assets/wfc-logo-white.svg?react";
 const Footer = () => {
   const renderSocialIcons = () => {
     return (
-      <div className="flex flex-row space-x-8">
-        <div className=" bg-white rounded-full max-h-14 flex items-center justify-center">
-          <FaFacebook className="h-14 w-auto text-[var(--color-primary)] -m-[1px]" />
-        </div>
-        <div className=" bg-[var(--color-primary)] max-h-14 w-14 rounded-full flex items-center justify-center">
-          <RiTwitterXFill className="h-8 w-auto text-white" />
-        </div>
-        <div className=" bg-[var(--color-primary)] max-h-14 w-14 rounded-full flex items-center justify-center">
-          <FaTiktok className="h-8 w-auto text-white" />
-        </div>
-        <div className=" bg-[var(--color-primary)] max-h-14 w-14 rounded-full flex items-center justify-center">
-          <FaInstagram className="h-8 w-auto text-white" />
-        </div>
+      <div className="flex flex-row items-center space-x-8">
+        <a href="https://www.facebook.com/womenforchangeSA" target="_blank" rel="noopener noreferrer">
+          <div className=" bg-white rounded-full h-14 flex items-center justify-center">
+            <FaFacebook className="h-14 w-auto text-[var(--color-primary)] -m-[1px]" />
+          </div>
+        </a>
+        <a href="https://twitter.com/womenforchange5" target="_blank" rel="noopener noreferrer">
+          <div className=" bg-[var(--color-primary)] h-14 w-14 rounded-full flex items-center justify-center">
+            <RiTwitterXFill className="h-8 w-auto text-white" />
+          </div>
+        </a>
+        <a href="https://www.tiktok.com/@womenforchange.sa" target="_blank" rel="noopener noreferrer">
+          <div className=" bg-[var(--color-primary)] h-14 w-14 rounded-full flex items-center justify-center">
+            <FaTiktok className="h-8 w-auto text-white" />
+          </div>
+        </a>
+        <a href="https://www.instagram.com/womenforchangesa/" target="_blank" rel="noopener noreferrer">
+          <div className=" bg-[var(--color-primary)] h-14 w-14 rounded-full flex items-center justify-center">
+            <FaInstagram className="h-8 w-auto text-white" />
+          </div>
+        </a>
       </div>
     );
   };
