2a7c3f1e6c33b65fa92751e9a9d019f14ed8be54
Mark Raffy <markraffybahalla@gmail.com>
2025-04-11 01:39:06 +0800

    Fix model positions

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -3,11 +3,14 @@ import { Suspense } from "react";
 import CasketAndBeads from "./CasketAndBeads";
 import UnburiableCasket from "@/assets/images/unburiable-casket.svg?react";
 import ModelLoader from "./ModelLoader";
+import useScreenSize from "@/utils/useScreenSize";
 
 const Banner = () => {
+  const { screenSizeCategory } = useScreenSize();
+
   const renderBackground = () => {
     return (
-      <div className="lg:h-screen h-[80vh] flex justify-center items-start pt-50">
+      <div className="xl:h-[110vh] lg:h-[65vh] h-[80vh] flex justify-center items-start pt-50">
         <UnburiableCasket className="xl:w-[60vw] lg:w-[80vw] w-[95vw]" />
       </div>
     );
@@ -18,7 +21,10 @@ const Banner = () => {
       <Canvas camera={{ position: [0, 0, 10] }} className="absolute inset-0 z-99 ">
         <ambientLight intensity={3} />
         <Suspense fallback={<ModelLoader />}>
-          <CasketAndBeads scale={1} position={[0, -2, 0]} />
+          <CasketAndBeads
+            scale={screenSizeCategory == "medium" ? 0.7 : 0.8}
+            position={[0, screenSizeCategory == "small" ? -1 : 0, 0]}
+          />
         </Suspense>
       </Canvas>
     );
@@ -27,7 +33,7 @@ const Banner = () => {
   return (
     <div>
       {renderBackground()}
-      <div className="lg:h-screen h-[80vh] w-screen absolute inset-0 z-1">{renderCanvas()}</div>
+      <div className="xl:h-[110vh] lg:h-[65vh] h-[80vh] w-screen absolute inset-0 z-1">{renderCanvas()}</div>
     </div>
   );
 };
