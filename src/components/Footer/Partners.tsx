b8debb88e8589f6690d6e5403a121261daf8caed
Mark Raffy <markraffybahalla@gmail.com>
2025-04-14 23:52:15 +0800

    Change partner logos sizes

--- src/components/Footer/Partners.tsx
+++ src/components/Footer/Partners.tsx
@@ -18,45 +18,45 @@ const Partners = () => {
 
   const renderPartnersLogos = () => {
     return (
-      <div className="lg:mt-20 mt-15 grid xl:grid-cols-3 lg:grid-cols-2 grid-cols-1 gap-14  justify-items-center px-[10vw]">
+      <div className="lg:mt-20 mt-15 grid xl:grid-cols-3 lg:grid-cols-2 grid-cols-1 gap-20 gap-x-32  justify-items-center px-[10vw]">
         <div className="flex flex-col space-y-2 items-center">
           <p className="text-white text-center">Stage Sponsor:</p>
-          <a href="https://amazishetribe.com/" target="_blank" rel="noopener noreferrer">
-            <img src={amaziSheTribe} alt="Amazi SHEtribe" className="lg:h-18 h-16 w-auto" />
+          <a href="https://amazishetribe.com/" target="_blank" rel="noopener noreferrer" className="flex-grow ">
+            <img src={amaziSheTribe} alt="Amazi SHEtribe" className="h-32 w-62 object-contain" />
           </a>
         </div>
         <div className="flex flex-col space-y-2 items-center">
           <p className="text-white text-center">Casket:</p>
-          <a href="https://enzowooddesign.co.za/" target="_blank" rel="noopener noreferrer">
-            <img src={enzoLogo} alt="Enzo Wood Designs" className="lg:h-18 h-16 w-auto" />
+          <a href="https://enzowooddesign.co.za/" target="_blank" rel="noopener noreferrer" className="flex-grow ">
+            <img src={enzoLogo} alt="Enzo Wood Designs" className="h-32 w-62 object-contain" />
           </a>
         </div>
 
         <div className="flex flex-col space-y-2 items-center">
           <p className="text-white text-center">Herse:</p>
-          <a href="https://randfunerals.co.za/" target="_blank" rel="noopener noreferrer">
-            <img src={randFuneralsLogo} alt="Rand Funerals" className="lg:h-18 h-16 w-auto" />
+          <a href="https://randfunerals.co.za/" target="_blank" rel="noopener noreferrer" className="flex-grow ">
+            <img src={randFuneralsLogo} alt="Rand Funerals" className="h-32 w-62 object-contain" />
           </a>
         </div>
 
         <div className="flex flex-col space-y-2 items-center">
           <p className="text-white text-center">Creative and PR:</p>
-          <a href="https://www.edelman.com/" target="_blank" rel="noopener noreferrer">
-            <img src={edelmanLogo} alt="Edelman" className="lg:h-18 h-16 w-auto" />
+          <a href="https://www.edelman.com/" target="_blank" rel="noopener noreferrer" className="flex-grow ">
+            <img src={edelmanLogo} alt="Edelman" className="h-32 w-62 object-contain" />
           </a>
         </div>
 
         <div className="flex flex-col space-y-2 items-center">
           <p className="text-white text-center">Print:</p>
-          <a href="https://imprintmedia.co.za/" target="_blank" rel="noopener noreferrer">
-            <img src={imprintMediaLogo} alt="Imprint Media" className="lg:h-18 h-16 w-62 object-contain" />
+          <a href="https://imprintmedia.co.za/" target="_blank" rel="noopener noreferrer" className="flex-grow ">
+            <img src={imprintMediaLogo} alt="Imprint Media" className="h-32 w-62 object-contain" />
           </a>
         </div>
 
         <div className="flex flex-col space-y-2 items-center">
           <p className="text-white text-center">Web:</p>
-          <a href="https://www.bigbrave.digital/" target="_blank" rel="noopener noreferrer">
-            <img src={bigBraveLogo} alt="Big Brave Digital" className="lg:h-18 h-16  w-62 object-contain" />
+          <a href="https://www.bigbrave.digital/" target="_blank" rel="noopener noreferrer" className="flex-grow ">
+            <img src={bigBraveLogo} alt="Big Brave Digital" className="h-full w-62 object-contain" />
           </a>
         </div>
       </div>
